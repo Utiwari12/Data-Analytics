e7009a01d780fd8f2faf93e71dc994b2d44839d2
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-07 15:28:23 +0530

    Loop Problem

---
+++ loopProb.py
@@ -0,0 +1,13 @@
+#write a program to find a sum of all the even numbers upto 100
+# n = 0 #start
+# sum = 0
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         sum += i
+# print(sum)
+
+#Write a program to write first 20 numbers and their squared numbers
+n = 1
+for i in range(1, 21):
+    print(n, n * n)
+    n += 1
