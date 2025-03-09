0ccfed54264308506bb56a76dfa160bd784bab24
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-09 23:14:55 +0530

    strings slice

---
+++ stringsSlice.py
@@ -0,0 +1,11 @@
+a = "Hello you are going to Patna for holy festival"
+# print(a[0:5])
+# print(a[6:])
+# print(a[6:22])
+#print(a[-8:])
+
+b = "0123456789"
+print(b[::2])
+print(b[::-1])
+print(b[:7:1])
+print(b[6::-1])
