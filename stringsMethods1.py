ff0f9ce64361b77b5a2cd211a644b78b80a38760
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-09 10:42:45 +0530

    String manupulation

---
+++ stringsMethods1.py
@@ -0,0 +1,34 @@
+a = "he is going to Patna for holy festival"
+print(a)
+
+#to get the length of string
+print(len(a))
+
+#find the count of characters in string
+#print(a.count("o"))
+
+#to convert each character to upper case
+#print(a.upper())
+
+#to convert each character to lower case
+#print(a.lower())
+
+#to convert first character to upper case
+#print(a.capitalize())
+
+#print(a.title())
+
+#to find the index number of any character
+# print(a.index("o"))
+# print(a.index("o", 10, 39))
+
+#to find the first letter to capital
+#print(a.capitalize())
+
+#to convert a string into lowercase
+#print(a.casefold())
+
+#to write variable inside a string
+x = "he is going to {} for holy festival"
+b = "Patna"
+print(x.format(b))
