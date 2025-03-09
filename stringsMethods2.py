725864e6dbdc3d0c7b9fb2026a5607d30cbf3936
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-09 13:55:30 +0530

    strings function

---
+++ stringsMethods2.py
@@ -0,0 +1,13 @@
+a = "hello"
+b= "Helo123"
+c = "123456"
+d= "HELLO"
+e = ""
+f = "Hello 123@"
+g = "1.234"
+#print(a.replace("hello", "hi"))
+print(a,a.isalnum())
+print(a.isalpha())
+print(b.isalnum())
+print(c , c.isalnum())
+print(f, f.isalnum())
