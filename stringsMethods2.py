ce59f7c8b7cc46c30c5ceb63a2807e052449258a
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-09 20:49:55 +0530

    Strings method

--- stringsMethods2.py
+++ stringsMethods2.py
@@ -2,12 +2,43 @@ a = "hello"
 b= "Helo123"
 c = "123456"
 d= "HELLO"
-e = ""
-f = "Hello 123@"
+e = " "
+f = "Hello 123"
 g = "1.234"
 #print(a.replace("hello", "hi"))
-print(a,a.isalnum())
-print(a.isalpha())
-print(b.isalnum())
-print(c , c.isalnum())
-print(f, f.isalnum())
+# print(a,a.isalnum())
+# print(a.isalpha())
+# print(b.isalnum())
+# print(c , c.isalnum())
+# print(f, f.isalnum())
+
+#isalpha - returns true if all characters in string are alphabetic
+# print(a, a.isalpha())
+# print(b, b.isalpha())
+#isalnum - returns true if all characters in string are alpha numeric
+#isnumeric - returns true if all characters in string are numeric
+# print(c, c.isnumeric())
+# print(e, e.isnumeric())
+#isdecimal - returns true if all characters in string are decimal
+# print(g, g.isdecimal())
+# print(c, c.isdecimal())
+#isdigit - returns true if all characters in string are digits
+# print(c, c.isdigit())
+# print(g, g.isdigit())
+# print(b, b.isdigit())
+#isspace - returns true if all characters in string are whitespace
+# print(e, e.isspace())
+# print(f, f.isspace())
+#isprintable - returns true if all characters in string are printable
+#islower - returns true if all characters in string are lowercase
+# print(a, a.islower())
+# print(d, d.islower())
+# print(b, b.islower())
+#isupper - returns true if all characters in string are uppercase
+# print(a, a.isupper())
+# print(d, d.isupper())
+
+#istitle - Returns true if the string follows the rules of a title
+print(a, a.istitle())
+print(d, d.istitle())
+print(f, f.istitle())
