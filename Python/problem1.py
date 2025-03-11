78691978b8c20d758e41082effd361ff920b16e5
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-11 20:28:36 +0530

    Problem2

--- Python/problem1.py
+++ Python/problem1.py
@@ -46,35 +46,135 @@
 
 #Write a program to create an area calculator for different shapes
 #area calculator
-print("Area Calculator")
-while True:
+# print("Area Calculator")
+# while True:
     
-    print("""press 1 to get the area of square
-    press 2 to get the area of rectangle
-    press 3 to get the area of circle
-    press 4 to get the area of triangle""")
-    choice = int(input("Enter your choice number between 1-4 : "))
-    if choice == 1:
-        while True:
-            side = float(input("Enter the side of square: "))
-            area = side * side
-            print("The area of square is: ", area)
-            repeat = input("do you want to try again? ")
-            if repeat == "yes":
-                continue
-            else:
-                break
-    elif choice == 2:
-        length = float(input("Enter the length of rectangle: "))
-        width = float(input("Enter the width of rectangle: "))
-        area = length * width
-        print("The area of rectangle is: ", area)
-    elif choice == 3:
-        radius = float(input("Enter the radius of circle: "))
-        area = 3.14 * radius * radius
-        print("The area of circle is: ", area)
-    elif choice == 4:
-        base = float(input("Enter the base of triangle: "))
-        height = float(input("Enter the height of triangle: "))
-        area = 0.5 * base * height
-        print("The area of triangle is: ", area)
+#     print("""press 1 to get the area of square
+#     press 2 to get the area of rectangle
+#     press 3 to get the area of circle
+#     press 4 to get the area of triangle""")
+#     choice = int(input("Enter your choice number between 1-4 : "))
+#     if choice == 1:
+#         while True:
+#             side = float(input("Enter the side of square: "))
+#             area = side * side
+#             print("The area of square is: ", area)
+#             repeat = input("do you want to try again? ")
+#             if repeat == "no":
+#                 break
+#             else:
+#                 continue
+
+#     elif choice == 2:
+#         while True:
+#            length = float(input("Enter the length of rectangle: "))
+#            width = float(input("Enter the width of rectangle: "))
+#            area = length * width
+#            print("The area of rectangle is: ", area)
+#            repeat = input("do you want to try again? ")
+#            if repeat == "no":
+#                break
+#            else:
+#                continue
+              
+             
+
+#     elif choice == 3:
+#         while True:
+#           radius = float(input("Enter the radius of circle: "))
+#           area = 3.14 * radius * radius
+#           print("The area of circle is: ", area)
+#           repeat = input("do you want to try again? ")
+#         if repeat == "no":
+#               break
+#         else:
+#             continue
+            
+
+#     elif choice == 4:
+#         while True:
+#           base = float(input("Enter the base of triangle: "))
+#           height = float(input("Enter the height of triangle: "))
+#           area = 0.5 * base * height
+#           print("The area of triangle is: ", area)
+#           repeat = input("do you want to try again? ")
+#         if repeat == "no":
+#               break
+#         else:
+#             continue
+#  repeat1 = input("do you want to try again? ")
+#  if repeat1 == "no":
+#      break
+#  else:
+#     continue
+    
+# print("Area Calculator")
+
+# while True:
+#     print("""press 1 to get the area of square
+#     press 2 to get the area of rectangle
+#     press 3 to get the area of circle
+#     press 4 to get the area of triangle""")
+#     choice = int(input("Enter your choice number between 1-4 : "))
+
+#     def calculate_area():
+#         if choice == 1:
+#             side = float(input("Enter the side of square: "))
+#             area = side * side
+#         elif choice == 2:
+#             length = float(input("Enter the length of rectangle: "))
+#             width = float(input("Enter the width of rectangle: "))
+#             area = length * width
+#         elif choice == 3:
+#             radius = float(input("Enter the radius of circle: "))
+#             area = 3.14 * radius * radius
+#         elif choice == 4:
+#             base = float(input("Enter the base of triangle: "))
+#             height = float(input("Enter the height of triangle: "))
+#             area = 0.5 * base * height
+#         print("The area is: ", area)
+#         return area
+
+#     calculate_area()
+
+#     repeat = input("do you want to try again? ")
+#     if repeat == "no":
+#         break
+#     else:
+#         print("Thank you")
+                
+
+#Problem 
+#a = "OOTD.YOLO.ASAP.BRB.GTG.OTW"
+
+#1.Write a program to sepatate the above string into coma(,) separated string
+# a = "OOTD.YOLO.ASAP.BRB.GTG.OTW"
+# b = a.split(".")
+# print(b)
+#2.Write a program to sort strings alphabetically in python
+# b = sorted(a)
+# print(b)
+# a = input("Enter any string here: ")
+# b = sorted(a)
+# print(b)
+#3.Write a program to remove a given character from string
+# x = "hello"
+# y = x.replace("l", "")
+# print(y)
+
+#z = "F.R.I.E.N.D.S."
+
+
+#4.Write a program to remove dot(.) from the above string
+# z = "F.R.I.E.N.D.S."
+
+# w = z.replace(".", "")
+# print(w)
+#5. Write a program to check the number of occurrence of a character in a substring
+
+c = "He is going to Delhi for holy festival and back to Delhi"
+
+d = c.count("Delhi")
+print(d)
+
+
---
+++ Python/problem2.py
@@ -0,0 +1,39 @@
+#1. Take a input from a user as a string then reverse it and print it
+
+# a = input("Enter any string here:")
+# b = a[::-1]
+# print(b)
+#2. Write a program to check if a string contans only digits
+# a = input("Enter any string here:")
+# b = a.isdigit()
+# print(b)
+
+#3. Write a program to chech if a string is palindrome
+# a = input("Enter any string here:")
+# b = a.lower()
+# c = b[::-1]
+# if b == c:
+#     print("String is palindrome")
+# else:
+#     print("String is not palindrome")
+
+#4. Write a program to find the number of vowels in a string
+# a = input("Enter any string here:")
+# b = a.lower()
+# c = b.count("a") + b.count("e") + b.count("i") + b.count("o") + b.count("u")
+# print(c)
+
+#5. Write a program to find the number of consonants in a string
+# a = input("Enter any string here:")
+# b = a.lower()
+# c = b.count("a") + b.count("e") + b.count("i") + b.count("o") + b.count("u")
+# d = len(a) - c
+# print(d)
+
+#6. Write a program to check if every word in a string starts with a capital letter
+# a = input("Enter any string here:")
+# b = a.title()
+# print(b)
+
+
+
