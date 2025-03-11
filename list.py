8aab98585a3d0483729ee31df0540f99851d9cd8
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-11 21:42:48 +0530

    List slice

---
+++ list.py
@@ -0,0 +1,14 @@
+cars = ["tata", "maruti", "toyota", "honda", 12, 15, 20.1]
+# print(cars)
+# print(cars[2])  #list slice
+# print(cars[-1])
+# print(cars[0:3])
+# print(cars[2:])
+# print(cars[:3])
+# print(cars[:])
+# print(cars[-2:])
+# print(cars[::2]) #gap of 2
+#print(cars[-6:-1])
+#print(cars[::-1])
+#print(cars, type(cars))
+print(cars[-1:-5:-1])
