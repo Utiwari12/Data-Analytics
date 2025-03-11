02f780395506562ad4d5e8b205757a2e7ac1cb1c
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-11 23:20:58 +0530

    List Iteration

---
+++ Python/listIteration.py
@@ -0,0 +1,23 @@
+#Iteration using for loop
+# cars = ["tata", "maruti", "toyota", "honda", 12, 15, 20.1]
+# for car in cars:
+#     print(car)
+
+#Iteration using for loop with range and lenth function
+# cars = ["tata", "maruti", "toyota", "honda", 12, 15, 20.1]
+# for i in range(len(cars)):
+#     print(cars[i])
+
+#Iteration using while loop
+# cars = ["tata", "maruti", "toyota", "honda", 12, 15, 20.1]
+# i = 0
+# while i < len(cars):
+#     print(cars[i])
+#     i = i + 1
+    #i += 1     #i = i + 1 both are same
+    
+#Using short hand notation for loop
+cars = ["tata", "maruti", "toyota", "honda", 12, 15, 20.1]
+# [print(i) for i in cars]
+#both code are same as below
+[print(car) for car in cars]
