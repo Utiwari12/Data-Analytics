9b3c6a500910ab6fe93ce1a480d287f14c474cc5
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-12 20:36:47 +0530

    List Function part1

---
+++ Python/listFunction1.py
@@ -0,0 +1,23 @@
+#To find the length of the list
+a = ["tata", "maruti", "toyota", "honda", "maruti"]
+#print(len(a))
+
+#To count an occurence of a particular element
+#b = a.count("maruti")
+#print(b)
+
+#To add the list
+# a.append("hero")  #append() is used for last location
+# print(a)
+#To add a specific location
+# a.insert(2, "patna")  # insert() is used for specific location Here 2 is the index number
+# print(a)
+#To remove the list
+a.remove("maruti")
+print(a)
+
+#To remove from a certain location
+a.pop(1)  #Here 1 is the index number
+#print(a.pop(1))
+print(a)
+
