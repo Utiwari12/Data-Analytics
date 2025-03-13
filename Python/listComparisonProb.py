c8f8671a996bf4dcd4c2f59699e56399e9c240f7
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-13 20:45:40 +0530

    List Comparision Problem

---
+++ Python/listComparisonProb.py
@@ -0,0 +1,32 @@
+#a = ["Rose", "Lily", "Jasmine", "Marigold"]
+
+#1. Write a program to swap first and last element of a list
+# a[0], a[-1] = a[-1], a[0]
+#the above also written as below
+#a[0], a[3] = a[3], a[0]
+# print(a)
+#2. Write a program to add a new element at the second position
+# a.insert(1, "Sunflower") # here 1 is the index number
+# print(a)
+
+#3. Write a program to delete an element from 3rd position
+# a.pop(2) # here 2 is the index number
+# print(a)
+
+b = [10 , 6 , 8 , 7 , 9]
+#4. Write a program to multiply all the elements in the list
+# mul = 1
+# for i in b:
+#     #mul*=i
+#     mul = mul * i
+# print(mul)
+
+#5. Write a program to find the largest number in the list
+print(max(b))
+#6. Write a program to find the smallest number in the list
+print(min(b))
+
+
+    
+
+
