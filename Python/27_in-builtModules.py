98f3fec370b0bc24124368c7f129f4f021483cd3
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-14 23:16:30 +0530

    math

---
+++ Python/27_in-builtModules.py
@@ -0,0 +1,32 @@
+#date and time
+import datetime
+# x = datetime.datetime.now()
+# print(x)
+
+y = datetime.datetime(2022, 1, 1)
+# print(y)
+# print(y.strftime("%A")) #print the day
+# print(y.strftime("%d")) #print the date
+# print(y.strftime("%m")) #print the month
+# print(y.strftime("%B")) #print the month
+# print(y.strftime("%Y")) #print the year
+# print(y.strftime("%y")) #print the year
+# print(y.strftime("%w")) #print the week
+# print(y.strftime("%a")) #print the day
+
+#Random
+import random
+# x =random.randint(1, 10)
+# print(x)
+
+l= ["Heads", "Tails"]
+x = random.choice(l)
+print(x)
+
+#Math
+import math
+# x = math.sqrt(25)
+# print(x)
+# eqc = math.pow(2, 3)
+# print(eqc)
+
