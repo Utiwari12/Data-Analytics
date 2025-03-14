e599fd6628b4224e34b8893aee60ab244f367156
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-14 17:45:48 +0530

    set

---
+++ 23_sets.py
@@ -0,0 +1,68 @@
+a = {"apple", "banana", "cherry", 4, 5, 6.5}
+b = {"apple", "banana", "guava", 4, 6, 6.5}
+#print(a, type(a))
+
+# for x in a:
+#     print(x, type(x))
+
+#Set Functions
+#add() - adds an element to the set
+# a.add("mango")
+# print(a)
+
+
+#remove() - removes the specified element from the set
+# a.remove("apple")
+# print(a)
+
+#discard() - removes the specified element from the set
+# a.discard("apple")
+# print(a)
+
+#pop() - removes an element from the set
+# a.pop()
+# print(a)
+
+#clear() - removes all the elements from the set
+# a.clear()
+# print(a)
+
+#union() - returns a set that contains all items from both sets
+# c = a.union(b)
+# print(c)
+
+#update() - updates the set with the union of this set and others
+# a.update(b)
+# print(a)
+
+#intersection() - returns a set that contains the intersection of two or more sets
+# c = a.intersection(b)
+# print(c)
+
+#intersection_update() - updates the set with the intersection of this set and others
+# a.intersection_update(b)
+# print(a)
+
+#differnce() - returns a set that contains the difference between two or more sets
+# c = a.difference(b)
+# print(c)
+# d = b.difference(a)
+# print(d)
+
+#difference_update() - updates the set with the difference between this set and another
+# a.difference_update(b)
+# print(a)
+# b.difference_update(a)
+# print(b)
+
+#symmetric_difference() - returns a set that contains the symmetric difference of two sets
+# c = a.symmetric_difference(b)
+# print(c)
+# d = b.symmetric_difference(a)
+# print(d)
+
+#symmetric_difference_update() - updates the set with the symmetric difference from this set and another
+# a.symmetric_difference_update(b)
+# print(a)
+# b.symmetric_difference_update(a)
+# print(b)
