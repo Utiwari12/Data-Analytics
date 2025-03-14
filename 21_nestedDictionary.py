7b3a6036f2c5e7c0c3f68bf41197eb4e0d3d2525
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-14 15:38:01 +0530

    Nested Dictionary

---
+++ 21_nestedDictionary.py
@@ -0,0 +1,22 @@
+employee = {
+    "emp1": {
+        "name": "John",
+        "age": 25,
+        "salary": 50000
+    },
+    "emp2": {
+        "name": "Emma",
+        "age": 30,
+        "salary": 48000
+    },
+    "emp3": {
+        "name": "Brad",
+        "age": 35,
+        "salary": 60000
+    }
+}
+
+#print(employee)
+print(employee["emp1"])
+print(employee["emp2"])
+print(employee["emp3"]["name"])
