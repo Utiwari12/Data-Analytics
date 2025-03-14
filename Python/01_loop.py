7ea66d1ca8a1e4c8ba3e61f619c1fdedd1398c06
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-14 14:33:37 +0530

    set sequence order

---
+++ Python/01_loop.py
@@ -0,0 +1,14 @@
+# for i in range(1,6):
+    
+#     print("Hello World")
+# for i in range(1,6, 2): #print of odd numbers
+#     print(i)
+# for i in range(2,6, 2): #print of even numbers
+#     print(i)
+    
+#print a table
+#n = 8 #print the table of 8
+n= int(input("enter a number here: ")) #prtint a table of any number
+for i in range(1,12): # i is a variable
+    
+        print(n,"x",i,"=",n*i)
