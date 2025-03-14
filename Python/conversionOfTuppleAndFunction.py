151f7c91481a3d577b427dc484d49095622a859f
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-14 13:28:25 +0530

    Conversion of tuple from list and vice versa

---
+++ Python/conversionOfTuppleAndFunction.py
@@ -0,0 +1,19 @@
+a = ("Nokia", "Samsung", "Redmi")
+#print("before conversion", a, type(a))
+#conversion of tupple to list
+# a = list(a)
+# print("after conversion", a, type(a))
+# a.append("oneplus")
+# a.insert(1, "vivo")
+# print(a, type(a))
+
+#conversion of list to tupple
+# a = tuple(a)
+# print(a, type(a))
+
+#count of element in tuple
+
+print(a.count("Nokia"))
+print(a.index("Nokia"))
+
+
