584c0a6e145ac329122e4939c463ec98bf043b4b
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-04-21 11:54:25 +0530

    add

---
+++ Python/05_user_input/02_add.py
@@ -0,0 +1,16 @@
+# a = input("enter a number here: ")
+# b = input("enter a number here: ")
+# a = int(a) #convert string to integer
+
+# b = int(b) #convert string to integer
+# print(a + b)
+#print(int(a) + int(b))
+
+#Another method
+# a = int(input("enter first number here: "))
+# b = int(input("enter second number here: "))
+# print(a + b)
+
+name = input("enter your name here: ")
+age = input("enter your age here: ")
+print(f"your name is {name} and your age is {age}")
